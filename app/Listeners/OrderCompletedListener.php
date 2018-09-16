afcfc4668c68658a09bc5ed86f46907c87e90ae0
Tobias Sundgren <Tobias.Sundgren@collectorbank.se>
2018-09-16 17:53:26 +0200

    Test Swish

--- app/Listeners/OrderCompletedListener.php
+++ app/Listeners/OrderCompletedListener.php
@@ -44,6 +44,41 @@ class OrderCompletedListener implements ShouldQueue
             new SendOrderTickets($event->order)
         ])->dispatch();
 
+        GetSwish($event->order);
         $this->dispatch(new SendOrderNotification($event->order));
+    }
+
+    public function GetSwish($order)
+    {
+            $service_url = 'https://mpc.getswish.net/qrg-swish/api/v1/prefilled';
+            $curl = curl_init($service_url);
+            $jsonData = array(
+                    "size"=> 300,
+                    "format"=>"png",
+                    "payee" => array("value" => "0739022421", "editable" => "false"),
+                    "amount" => array("value" => 100, "editable" => "false"),
+                    "message" => array("value" => "1ABC123123",  "editable" => "false"}
+                  }
+            );
+            $headers = array('Content-type: application/json');
+            $jsonDataEncoded = json_encode($jsonData);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+            $curl_response = curl_exec($curl);
+            if ($curl_response === false) {
+                $info = curl_getinfo($curl);
+                curl_close($curl);
+                die('error occured during curl exec. Additioanl info: ' . var_export($info));
+            }
+            curl_close($curl);
+            $file_path = public_path(config('attendize.event_pdf_tickets_path')) . '/swish//' . $file_name;
+            file_put_contents(file_path, curl_response);
+            $decoded = json_decode($curl_response);
+            if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+                die('error occured: ' . $decoded->response->errormessage);
+            }
+            echo 'response ok!';
     }
 }
