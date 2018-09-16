1ef313438780c178248f92704d8b3468159b987a
Tobias Sundgren <Tobias.Sundgren@collectorbank.se>
2018-09-16 09:44:23 +0200

    Dont attach tickets if not paid

--- app/Mailers/OrderMailer.php
+++ app/Mailers/OrderMailer.php
@@ -36,19 +36,26 @@ class OrderMailer
             'order' => $order,
             'orderService' => $orderService
         ];
+        if($order->is_payment_received)
+        {
+            $file_name = $order->order_reference;
+            $file_path = public_path(config('attendize.event_pdf_tickets_path')) . '/' . $file_name . '.pdf';
+            if (!file_exists($file_path)) {
+                Log::error("Cannot send actual ticket to : " . $order->email . " as ticket file does not exist on disk");
+                return;
+            }
 
-        $file_name = $order->order_reference;
-        $file_path = public_path(config('attendize.event_pdf_tickets_path')) . '/' . $file_name . '.pdf';
-        if (!file_exists($file_path)) {
-            Log::error("Cannot send actual ticket to : " . $order->email . " as ticket file does not exist on disk");
-            return;
+            Mail::send('Mailers.TicketMailer.SendOrderTickets', $data, function ($message) use ($order, $file_path) {
+                $message->to($order->email);
+                $message->subject(trans("Controllers.tickets_for_event", ["event" => $order->event->title]));
+                $message->attach($file_path);
+            });
+        }else{
+            Mail::send('Mailers.TicketMailer.SendOrderTickets', $data, function ($message) use ($order, $file_path) {
+                $message->to($order->email);
+                $message->subject(trans("Controllers.tickets_for_event", ["event" => $order->event->title]));
+            });
         }
-
-        Mail::send('Mailers.TicketMailer.SendOrderTickets', $data, function ($message) use ($order, $file_path) {
-            $message->to($order->email);
-            $message->subject(trans("Controllers.tickets_for_event", ["event" => $order->event->title]));
-            $message->attach($file_path);
-        });
 
     }
 
