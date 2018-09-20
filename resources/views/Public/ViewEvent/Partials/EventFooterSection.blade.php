7ad99b70a62bbd5bc3e602f6cd3b5363ab71d437
Tobias Sundgren <Tobias.Sundgren@collectorbank.se>
2018-09-20 21:06:04 +0200

    test modal

--- resources/views/Public/ViewEvent/Partials/EventFooterSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventFooterSection.blade.php
@@ -4,6 +4,29 @@
             <div class="col-md-12">
             &bull; <a href="#">Allmänna vilkor</a>
             &bull; <a href="#">Kontakta oss</a> 
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+            Allmänna vilkor
+            </button>
+            <!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
                 {{--Attendize is provided free of charge on the condition the below hyperlink is left in place.--}}
                 {{--See https://www.attendize.com/licence.php for more information.--}}
