7fd588e7dff88882397087747e48be7f627d0bb2
Tobias Sundgren <Tobias.Sundgren@collectorbank.se>
2018-09-26 16:18:48 +0200

    Translations

--- resources/lang/sv/Javascript.php
+++ resources/lang/sv/Javascript.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-    "processing" => "Just a second...",
-    "time_run_out" => "You have run out of time! You will have to restart the order process.",
-    "just_2_minutes" => "You only have 2 minutes left to complete this order!",
+    "processing" => "Skickar. Var vänlig vänta...",
+    "time_run_out" => "Tyvärr har det gått för lång tid! Du måste börja om med din beställning.",
+    "just_2_minutes" => "Du har bara 2 minuter kvar att slutföra din beställning!",
     "whoops" => 'Whoops!, it looks like the server returned an error.
                    Please try again, or contact the webmaster if the problem persists.',
     "whoops2" => 'Something went wrong! Refresh the page and try again',
     "whoops_and_error" => "Whoops!, something has gone wrong.<br><br>:code :error",
     "at_least_one_option" => 'You must have at least one option.',
-    "credit_card_error" => 'The credit card number appears to be invalid.',
-    "expiry_error" => 'The expiration date appears to be invalid.',
-    "cvc_error" => 'The CVC number appears to be invalid.',
-    "card_validation_error" => 'Please check your card details and try again.',
+    "credit_card_error" => 'Kortnumret är felaktigt.',
+    "expiry_error" => 'Ogiltigt datum på ditt kort.',
+    "cvc_error" => 'CVC numret är felaktigt.',
+    "card_validation_error" => 'Kontrollera kort-informationen och försök igen.',
     "checkin_init_error" => 'There was an error while initializing scanner. Check if you\'re connecting through secure page (https) and that your browser supports the scanner.',
 ];
