993f8da5c2e11781f47bcb2542bb2b3dca264b41
Tobias Sundgren <Tobias.Sundgren@collectorbank.se>
2018-09-28 12:17:26 +0200

    redirect to event

--- app/Http/Controllers/MyBaseController.php
+++ app/Http/Controllers/MyBaseController.php
@@ -15,7 +15,7 @@ class MyBaseController extends Controller
     {
 
         if (empty(Auth::user())) {
-            return redirect()->to('/login');
+            return redirect()->to('/e/3/det-svanger-om-taube');
         }
 
         /*
